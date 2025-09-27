f6699ab41cf91d6c268dd8360928bb43d562c574
Shubham Zawar <shubhamzawar55@gmail.com>
2025-09-27 14:54:35 +0530

    worked on init functionality

--- backend/controllers/init.js
+++ backend/controllers/init.js
@@ -1,6 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
 async function initRepo()
 {
-    console.log("init command called");
+    const repoPath = path.resolve(process.cwd(),".apnaGit");
+    const commitsPath = path.join(repoPath,"commits");
+
+    try{
+        await fs.mkdir(repoPath,{recursive:true});
+        await fs.mkdir(commitsPath,{recursive:true});
+        await fs.writeFile(
+            path.join(repoPath,"config.json"),
+            JSON.stringify({bucket:"s3 bucket"})
+        );
+        console.log("Repository initialized!");
+    }catch(err)
+    {
+        console.error("Error initalising the repository",err);
+    }
 }
 
 module.exports = {initRepo};
