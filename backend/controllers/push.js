eb89edd041f3cf8e9fe8408f09d4597547a4be44
Shubham Zawar <shubhamzawar55@gmail.com>
2025-09-28 13:58:02 +0530

    Worked on push function

--- backend/controllers/push.js
+++ backend/controllers/push.js
@@ -1,6 +1,35 @@
+const fs = require('fs').promises;
+const path = require('path');
+const {s3,S3_BUCKET} = require("../config/aws-config");
 async function pushRepo()
 {
-    console.log("pushing a repo");
+    const repoPath = path.resolve(process.cwd(),".apnaGit");
+    const commitsPath = path.join(repoPath,"commits");
+    try
+    {
+        const commitDirs = await fs.readdir(commitsPath);
+        for(commitDir of commitDirs)
+        {
+            const commitPath =  path.join(commitsPath,commitDir);
+            const files = await fs.readdir(commitPath);
+            for(file of files)
+            {
+                const filePath = path.join(commitPath,file);
+                const fileContent = await fs.readFile(filePath);
+                const params = {
+                    Bucket:S3_BUCKET,
+                    Key:`commits/${commitDir}/${file}`,
+                    Body:fileContent,
+
+                };
+                await s3.upload(params).promise();
+            }
+        }
+        console.log("All commits pushed to S3");
+    }catch(err)
+    {
+        console.error("Error pushing to s3:",err);
+    }
 }
 
 module.exports = {pushRepo};
