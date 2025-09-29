f7f0d5fabcf64790f3d9d365d9b832f60774f5be
Shubham Zawar <shubhamzawar55@gmail.com>
2025-09-29 09:31:30 +0530

    worked on revert functionality

--- backend/controllers/revert.js
+++ backend/controllers/revert.js
@@ -1,6 +1,27 @@
-async function revertRepo()
+const fs = require('fs');
+const path = require('path');
+const {promisify} = require('util');
+const readdir = promisify(fs.readdir);
+const copyFile = promisify(fs.copyFile);
+
+async function revertRepo(commitID)
 {
-    console.log("revert to a specific version");
+   const repoPath = path.resolve(process.cwd(),".apnaGit");
+   const commitsPath = path.join(repoPath,"commits");
+   try
+   {
+    const commitDir = path.join(commitsPath,commitID);
+    const files = await readdir(commitDir);
+    const parentDir = path.resolve(repoPath,"..");
+    for(const file of files)
+    {
+        await copyFile(path.join(commitDir,file),path.join(parentDir,file));
+    }
+    console.log(`Revert with ${commitID} commited successfully`);
+   }catch(err)
+   {
+    console.error("Unable to revert",err);
+   }
 }
 
 module.exports = {revertRepo};
--- backend/index.js
+++ backend/index.js
@@ -33,5 +33,7 @@ yargs(hideBin(process.argv))
         type:"string"
 
     })
-},revertRepo)
+},(argv)=>{
+    revertRepo(argv.commitID);
+})
 .demandCommand(1,"You need atleast one command").help().argv;
