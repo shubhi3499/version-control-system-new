06d7a3705a532dc922a516d2aaa04117cd703aa6
Shubham Zawar <shubhamzawar55@gmail.com>
2025-10-01 08:27:12 +0530

    created issue demo issue controller and router file

---
+++ backend/controllers/issueController.js
@@ -0,0 +1,27 @@
+const createIssue = (req,res)=>{
+    res.send("Issue created");
+};
+
+const updateIssueById = (req,res)=>{
+    res.send("Issue updated");
+};
+
+const deleteIssueById = (req,res)=>{
+    res.send("Issue deleted");
+};
+
+const getAllIssues = (req,res)=>{
+    res.send("All issues fetched");
+};
+
+const getIssueById = (req,res)=>{
+    res.send("Issue fetched");
+};
+
+module.exports = {
+    createIssue,
+    updateIssueById,
+    deleteIssueById,
+    getAllIssues,
+    getIssueById
+}
