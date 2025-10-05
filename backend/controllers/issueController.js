43bb250435b55b075b4660fad0e040da904ebca8
Shubham Zawar <shubhamzawar55@gmail.com>
2025-10-05 19:08:23 +0530

    worked on issue creation

--- backend/controllers/issueController.js
+++ backend/controllers/issueController.js
@@ -1,20 +1,42 @@
-const createIssue = (req,res)=>{
-    res.send("Issue created");
-};
+const mongoose = require('mongoose');
+const Repository = require("../models/repoModel");
+const User = require("../models/userModel");
+const Issue = require("../models/issueModel");
 
-const updateIssueById = (req,res)=>{
+async function createIssue (req,res)
+{
+    const {title,description} = req.body;
+    const {id} = req.params;
+
+    try{
+    const issue = new Issue({
+        title,
+        description,
+        repository:id,
+    });
+    await issue.save();
+    res.status(201).json(issue);
+    }
+    catch (err) {
+        console.error("Error during issue creation", err.message);
+        res.status(500).send("Server Error");
+    }
+
+}
+
+async function updateIssueById (req,res){
     res.send("Issue updated");
 };
 
-const deleteIssueById = (req,res)=>{
+async function deleteIssueById  (req,res){
     res.send("Issue deleted");
 };
 
-const getAllIssues = (req,res)=>{
+async function getAllIssues  (req,res){
     res.send("All issues fetched");
 };
 
-const getIssueById = (req,res)=>{
+async function getIssueById  (req,res){
     res.send("Issue fetched");
 };
 
