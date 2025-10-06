0d1affa67c2b0e0c8003f774b56f7963f6a44748
Shubham Zawar <shubhamzawar55@gmail.com>
2025-10-06 10:15:14 +0530

    worked on routes file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,11 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AuthProvider } from './authContext.jsx'
+import { AuthProvider } from './authContext.jsx';
+import ProjectRoutes from './Routes.jsx';
+import {BrowserRouter as Router} from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
     <AuthProvider>
-    <App />
+    <Router>
+    <ProjectRoutes />
+    </Router>
     </AuthProvider>
   
 );
